6544b7cb01958cd44dc435ba63312423d5fc5a09
HanderWei <hander_wei@163.com>
2015-08-25 14:19:07 +0800

    Create Project

---
+++ sample/src/main/java/me/chen_wei/superrecyclerview/MainActivity.java
@@ -0,0 +1,17 @@
+package me.chen_wei.superrecyclerview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hander on 15/8/25.
+ * <p/>
+ * Email : hander_wei@163.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
